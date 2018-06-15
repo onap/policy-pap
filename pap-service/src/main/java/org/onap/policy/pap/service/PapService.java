d5dbd30ee536be86d3c0e2ffde5f1e93a1d6a94c
mmis <michael.morris@ericsson.com>
2018-06-15 15:53:34 +0100

    Architectural improvements prototype
    Prototype implementation of PAP in accordance with architectural
    improvements
    
    Issue-ID: POLICY-874
    
    Change-Id: Ifc496a8ee7cbd3f13fceca37e6325e297b1d7883
    Signed-off-by: mmis <michael.morris@ericsson.com>

---
+++ pap-service/src/main/java/org/onap/policy/pap/service/PapService.java
@@ -0,0 +1,190 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * pap-service
+ * ================================================================================
+ * Copyright (C) 2018 Ericsson Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pap.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.apache.commons.io.FileUtils;
+import org.onap.policy.pap.pdpclient.ApiClient;
+import org.onap.policy.pap.pdpclient.ApiException;
+import org.onap.policy.pap.pdpclient.PdpClient;
+import org.onap.policy.pap.pdpclient.model.Policy;
+import org.onap.policy.pap.service.dao.PolicyDbDao;
+import org.onap.policy.pap.service.dao.PolicyDbDaoTransaction;
+import org.onap.policy.pap.service.jpa.PdpGroupEntity;
+import org.onap.policy.pap.service.jpa.PolicyEntity;
+import org.onap.policy.pap.service.jpa.PolicySetToPolicyEntity;
+import org.onap.policy.pap.service.nexus.NexusRestSearchParameters;
+import org.onap.policy.pap.service.nexus.NexusRestWrapper;
+import org.onap.policy.pap.service.nexus.NexusRestWrapperException;
+import org.onap.policy.pap.service.nexus.pojo.NexusArtifact;
+import org.onap.policy.pap.service.nexus.pojo.NexusSearchResult;
+import org.onap.policy.pap.service.plugin.DummyPapPlugin;
+import org.onap.policy.pap.service.rest.model.PdpStatusParameters;
+import org.onap.policy.pap.service.rest.model.PdpStatusParameters.PdpStateEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PapService {
+
+    private static Logger logger = LoggerFactory.getLogger(PapService.class);
+
+    private static final String PERSISTENCE_UNIT = "PolicyDb";
+    private Properties properties = new Properties();
+
+    private PolicyDbDao policyDbDao;
+
+    /**
+     * Instantiate the PAP.
+     */
+    public PapService() {
+        try (InputStream input = new FileInputStream("src/main/resources/pap.properties")) {
+            properties.load(input);
+        } catch (Exception e) {
+            logger.error("Exception Occured while loading properties file" + e);
+        }
+
+        EntityManagerFactory entityManagerFactory =
+                Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, new Properties());
+        policyDbDao = PolicyDbDao.getPolicyDbDaoInstance(entityManagerFactory);
+    }
+
+    /**
+     * Handle a PDP status report.
+     * 
+     * @param pdpStatusParameters the status report paramaters from the PDP
+     * @throws NexusRestWrapperException if an error occurs downloading artifacts from maven
+     * @throws IOException if an error occurs writing maven artifacts to the local file system
+     * @throws ApiException if an error occurs sending a policy to the PDP
+     */
+    public void handlePdpStatusReport(final PdpStatusParameters pdpStatusParameters)
+            throws NexusRestWrapperException, IOException, ApiException {
+
+        if (pdpStatusParameters.getPdpState().equals(PdpStateEnum.PASSIVE)) {
+
+            PolicyDbDaoTransaction policyDbDaoTransaction = policyDbDao.getNewTransaction();
+
+            int policySetId = getPolicySetIdForPdpGroup(policyDbDaoTransaction, pdpStatusParameters.getPdpGroupName());
+            int pdpGroupId = getPdpGroupId(policyDbDaoTransaction, pdpStatusParameters.getPdpGroupName());
+            final Collection<PolicySetToPolicyEntity> policySetToPolicyEntitys =
+                    getPoliciesForPolicySetId(policyDbDaoTransaction, policySetId);
+
+
+            for (PolicySetToPolicyEntity policySetToPolicyEntity : policySetToPolicyEntitys) {
+                int policyId = policySetToPolicyEntity.getPolicyId();
+                logger.debug("PAP: Found policy Id for pdp group {} : {}", pdpStatusParameters.getPdpGroupName(),
+                        policyId);
+
+                final PolicyEntity policyEntity = getPolicyEntity(policyDbDaoTransaction, policyId);
+                Collection<File> policyArtifacts = downloadMavenArtifactsForPolicy(policyEntity);
+                logger.info("PAP: Sending policy maven artifact etc. to plugin");
+                Policy policy = generatePolicy(policyId, policyEntity.getPolicyName(), policyEntity.getPolicyVersion(),
+                        policyArtifacts, Collections.<String, String>emptyMap());
+                deployPolicyToPdp(pdpStatusParameters.getPdpEndpoint(), policy);
+            }
+
+            logger.info("PAP: Add new PDP to database");
+            policyDbDaoTransaction.createPdp(pdpStatusParameters.getPdpName(), pdpStatusParameters.getPdpVersion(),
+                    pdpStatusParameters.getPdpState().toString(), pdpStatusParameters.getPdpType(),
+                    pdpStatusParameters.getPdpEndpoint(), pdpGroupId, policySetId);
+
+            policyDbDaoTransaction.commitTransaction();
+            logger.info("PAP: transaction commited");
+        }
+    }
+
+    private int getPolicySetIdForPdpGroup(final PolicyDbDaoTransaction policyDbDaoTransaction,
+            final String pdpGroupName) {
+        PdpGroupEntity pdpGroupEntity = policyDbDaoTransaction.getPdpGroupEntity(pdpGroupName);
+        return pdpGroupEntity.getPolicySetId();
+    }
+
+    private Collection<PolicySetToPolicyEntity> getPoliciesForPolicySetId(
+            final PolicyDbDaoTransaction policyDbDaoTransaction, final int policySetId) {
+        return policyDbDaoTransaction.getPolicySetToPolicyEntities(policySetId);
+    }
+
+    private int getPdpGroupId(final PolicyDbDaoTransaction policyDbDaoTransaction, final String pdpGroupName) {
+        return policyDbDaoTransaction.getPdpGroupEntity(pdpGroupName).getGroupId();
+    }
+
+    private Collection<File> downloadMavenArtifactsForPolicy(final PolicyEntity policyEntity)
+            throws NexusRestWrapperException, IOException {
+        String mavenArtifactString = policyEntity.getPolicyMavenArtifact();
+        String[] mavenArtifactDetails = mavenArtifactString.split(":");
+
+        NexusRestSearchParameters nexusRestSearchParameters = new NexusRestSearchParameters();
+        nexusRestSearchParameters.useFilterSearch(mavenArtifactDetails[0], mavenArtifactDetails[1],
+                mavenArtifactDetails[2], null, null);
+
+        // NexusRestWrapper nexusRestWrapper = new
+        // NexusRestWrapper(properties.getProperty("nexus_server_url"),
+        // properties.getProperty("nexus_username"), properties.getProperty("nexus_password"));
+        NexusRestWrapper nexusRestWrapper = new NexusRestWrapper(properties.getProperty("nexus_server_url"));
+        NexusSearchResult nexusSearchResult = nexusRestWrapper.findArtifact(nexusRestSearchParameters);
+        List<NexusArtifact> nexusArtifacts = nexusSearchResult.getArtifactList();
+
+        Collection<File> artifacts = new HashSet<>();
+        for (NexusArtifact nexusArtifact : nexusArtifacts) {
+            final String urlPath = nexusArtifact.getUrlPath();
+            logger.info("PAP: Downloading artifact: {}", urlPath);
+
+            final String fileName = urlPath.substring(urlPath.lastIndexOf('/') + 1);
+            final File file = new File(properties.getProperty("nexus_download_dir") + fileName + ".jar");
+            artifacts.add(file);
+
+            FileUtils.copyURLToFile(new URL(urlPath + ".jar"), file);
+
+            logger.debug("PAP: Downloaded artifact to: ", file.getAbsolutePath());
+        }
+        return artifacts;
+    }
+
+    private PolicyEntity getPolicyEntity(final PolicyDbDaoTransaction policyDbDaoTransaction, int policyId) {
+        return policyDbDaoTransaction.getPolicyEntity(policyId);
+    }
+
+    private Policy generatePolicy(int policyId, final String policyName, final String policyVersion,
+            final Collection<File> policyArtifacts, final Map<String, String> policyMetadata) {
+        DummyPapPlugin plugin = new DummyPapPlugin();
+        return plugin.generatePolicy(policyId, policyName, policyVersion, policyArtifacts, policyMetadata);
+    }
+
+    private void deployPolicyToPdp(String pdpEndpopint, Policy policy) throws ApiException {
+        logger.info("PAP: Sending policy to PDP", policy);
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(pdpEndpopint);
+        PdpClient pdpClient = new PdpClient(apiClient);
+        pdpClient.policyDeploymentDeployPut(policy);
+    }
+
+}
---
+++ pdp-client/src/main/java/org/onap/policy/pap/pdpclient/model/ModeParameters.java
@@ -0,0 +1,149 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * pdp-client
+ * ================================================================================
+ * Copyright (C) 2018 Ericsson Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pap.pdpclient.model;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * ModeParameters
+ */
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-06-05T10:23:28.596Z")
+public class ModeParameters {
+    /**
+     * Gets or Sets mode
+     */
+    @JsonAdapter(ModeEnum.Adapter.class)
+    public enum ModeEnum {
+        ACTIVE("active"),
+
+        SAFE("safe"),
+
+        TEST("test");
+
+        private String value;
+
+        ModeEnum(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+
+        public static ModeEnum fromValue(String text) {
+            for (ModeEnum b : ModeEnum.values()) {
+                if (String.valueOf(b.value).equals(text)) {
+                    return b;
+                }
+            }
+            return null;
+        }
+
+        public static class Adapter extends TypeAdapter<ModeEnum> {
+            @Override
+            public void write(final JsonWriter jsonWriter, final ModeEnum enumeration) throws IOException {
+                jsonWriter.value(enumeration.getValue());
+            }
+
+            @Override
+            public ModeEnum read(final JsonReader jsonReader) throws IOException {
+                String value = jsonReader.nextString();
+                return ModeEnum.fromValue(String.valueOf(value));
+            }
+        }
+    }
+
+    @SerializedName("mode")
+    private ModeEnum mode = null;
+
+    public ModeParameters mode(ModeEnum mode) {
+        this.mode = mode;
+        return this;
+    }
+
+    /**
+     * Get mode
+     * 
+     * @return mode
+     **/
+    @ApiModelProperty(required = true, value = "")
+    public ModeEnum getMode() {
+        return mode;
+    }
+
+    public void setMode(ModeEnum mode) {
+        this.mode = mode;
+    }
+
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ModeParameters modeParameters = (ModeParameters) o;
+        return Objects.equals(this.mode, modeParameters.mode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mode);
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class ModeParameters {\n");
+
+        sb.append("    mode: ").append(toIndentedString(mode)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces (except the first
+     * line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+
+}
+
