d68a2bdaefe7f8fea2678d80f39ce944d97e5c1d
Jim Hahn <jrh3@att.com>
2019-03-27 09:07:37 -0400

    Add and register DAO provider wrapper
    Added a DAO provider wrapper supporting a single create() method
    to create a DAO provider.  PapActivator registers it at start-up.
    
    Change-Id: Ie3abd7c7a4f9ffa7aa086609515a0bb3891585d9
    Issue-ID: POLICY-1542
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ main/src/main/java/org/onap/policy/pap/main/PolicyModelsProviderFactoryWrapper.java
@@ -0,0 +1,62 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pap.main;
+
+import org.onap.policy.models.base.PfModelException;
+import org.onap.policy.models.provider.PolicyModelsProvider;
+import org.onap.policy.models.provider.PolicyModelsProviderFactory;
+import org.onap.policy.models.provider.PolicyModelsProviderParameters;
+
+/**
+ * Wraps a {@link PolicyModelsProviderFactoryWrapper}.
+ */
+public class PolicyModelsProviderFactoryWrapper implements AutoCloseable {
+    private final PolicyModelsProviderParameters params;
+    private final PolicyModelsProviderFactory factory;
+
+    /**
+     * Constructs the object.
+     *
+     * @param params DAO configuration parameters
+     */
+    public PolicyModelsProviderFactoryWrapper(PolicyModelsProviderParameters params) {
+        this.params = params;
+        this.factory = new PolicyModelsProviderFactory();
+    }
+
+    @Override
+    public void close() throws Exception {
+        /*
+         * TODO PolicyModelsProviderFactory should, in theory, implement AutoCloseable so
+         * it can close the entity manager factory and release all data.
+         */
+    }
+
+    /**
+     * Creates a provider based on models-pap.
+     *
+     * @return a new provider
+     * @throws PfModelException if an error occurs
+     */
+    public PolicyModelsProvider create() throws PfModelException {
+        return factory.createPolicyModelsProvider(params);
+    }
+}
