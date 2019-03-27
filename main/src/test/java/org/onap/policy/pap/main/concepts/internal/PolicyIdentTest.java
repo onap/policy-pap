bc111064b4a06b300240fc64e6c37b29928a02c7
Jim Hahn <jrh3@att.com>
2019-03-27 11:33:24 -0400

    Added internal classes to mirror models-pap classes
    Instead of creating mapper classes, decided to put the conversion methods
    within the classes, themselves.  This makes it less likely that someone
    will forget to update the conversion method if fields are added or
    removed.  Unfortunately, this meant subclassing a few of the models-pdp
    classes (e.g., PolicyIdentXxx), to add the required conversion methods.
    On the positive side, it means that PAP code need only use the classes
    from "internal"; classes from models-pdp can be ignored.
    Renamed some of the variables to make it less likely to confuse them.
    Modified copyTo() methods to return the target object.
    
    Corrected a few IdentXxx tests.
    
    Change-Id: Ic5aa29fb46f93a90f5b97ea0fd4f6143ed7d5a32
    Issue-ID: POLICY-1542
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ main/src/test/java/org/onap/policy/pap/main/concepts/internal/PolicyIdentTest.java
@@ -0,0 +1,126 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP PAP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pap.main.concepts.internal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PolicyIdentTest {
+    private static final String NAME = "my-name";
+    private static final String VERSION = "10.11.12";
+
+    private PolicyIdent resp;
+    private PolicyIdent resp2;
+    private org.onap.policy.models.pap.concepts.PolicyIdent extresp;
+
+    @Before
+    public void setUp() {
+        resp = new PolicyIdent();
+        extresp = new org.onap.policy.models.pap.concepts.PolicyIdent();
+    }
+
+    @Test
+    public void testPolicyIdentPolicyIdent() {
+        // first check with null values
+        resp2 = new PolicyIdent(resp);
+        assertEquals(resp.toString(), resp2.toString());
+        assertTrue(resp2.isNullKey());
+        assertTrue(resp2.isNullVersion());
+
+        // populate it and check again
+        populate(resp);
+        resp2 = new PolicyIdent(resp);
+        assertEquals(resp.toString(), resp2.toString());
+        assertEquals(resp.getName(), resp2.getName());
+        assertEquals(resp.getVersion(), resp2.getVersion());
+    }
+
+    @Test
+    public void testPolicyIdent() {
+        assertTrue(resp.isNullKey());
+    }
+
+    @Test
+    public void testPolicyIdentExternalPolicyIdent() {
+        // first check with null values
+        resp2 = new PolicyIdent(extresp);
+        assertEquals(IdentUtil.NULL_NAME_INTERNAL, resp2.getName());
+        assertEquals(IdentUtil.NULL_VERSION_INTERNAL, resp2.getVersion());
+
+        // populate it and check again
+        populate(extresp);
+        resp2 = new PolicyIdent(extresp);
+        assertEquals(extresp.getName(), resp2.getName());
+        assertEquals(extresp.getVersion(), resp2.getVersion());
+    }
+
+    @Test
+    public void testToExternal() {
+        // first check with null values
+        extresp = resp.toExternal();
+        assertEquals(IdentUtil.NULL_NAME_EXTERNAL, extresp.getName());
+        assertEquals(IdentUtil.NULL_VERSION_EXTERNAL, extresp.getVersion());
+
+        // populate it and check again
+        populate(resp);
+        extresp = resp.toExternal();
+        assertEquals(resp.getName(), extresp.getName());
+        assertEquals(resp.getVersion(), extresp.getVersion());
+    }
+
+    @Test
+    public void testCopyToPolicyIdent() {
+        // first check with null values
+        resp.copyTo(extresp);
+        assertEquals(IdentUtil.NULL_NAME_EXTERNAL, extresp.getName());
+        assertEquals(IdentUtil.NULL_VERSION_EXTERNAL, extresp.getVersion());
+
+        // populate it and check again
+        populate(resp);
+        extresp = new org.onap.policy.models.pap.concepts.PolicyIdent();
+        resp.copyTo(extresp);
+        assertEquals(resp.getName(), extresp.getName());
+        assertEquals(resp.getVersion(), extresp.getVersion());
+    }
+
+    /**
+     * Populates all fields in the target.
+     *
+     * @param target target whose fields are to be set
+     */
+    private void populate(PolicyIdent target) {
+        target.setName(NAME);
+        target.setVersion(VERSION);
+    }
+
+    /**
+     * Populates all fields in the target.
+     *
+     * @param target target whose fields are to be set
+     */
+    private void populate(org.onap.policy.models.pap.concepts.PolicyIdent target) {
+        target.setName(NAME);
+        target.setVersion(VERSION);
+    }
+}
