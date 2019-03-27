58060d99244ea953626c08f7b901146304534ce4
Jim Hahn <jrh3@att.com>
2019-03-27 11:33:24 -0400

    Added internal classes to mirror models-pap classes
    Instead of creating mapper classes, decided to put the conversion methods
    within the classes, themselves.  This makes it less likely that someone
    will forget to update the conversion method if fields are added or
    removed.  Unfortunately, this meant subclassing a few of the models-pdp
    classes (e.g., PolicyIdentXxx), to add the required conversion methods.
    On the positive side, it means that PAP code need only use the classes
    from "internal"; classes from models-pdp can be ignored.
    
    Renamed some of the variables to make it less likely to confuse them.
    Modified copyTo() methods to return the target object.
    
    Change-Id: Ic5aa29fb46f93a90f5b97ea0fd4f6143ed7d5a32
    Issue-ID: POLICY-1542
    Signed-off-by: Jim Hahn <jrh3@att.com>

---
+++ main/src/main/java/org/onap/policy/pap/main/concepts/internal/PolicyIdentOptVersion.java
@@ -0,0 +1,69 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP PAP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pap.main.concepts.internal;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * Internal class providing conversion to/from the corresponding class in models-pap.
+ */
+@NoArgsConstructor
+public class PolicyIdentOptVersion extends org.onap.policy.models.pdp.concepts.PolicyIdentOptVersion {
+    private static final long serialVersionUID = 1L;
+
+    public PolicyIdentOptVersion(PolicyIdentOptVersion source) {
+        super(source);
+    }
+
+    /**
+     * Constructs the object, populating fields from the external source.
+     *
+     * @param source source from which to copy the data
+     */
+    public PolicyIdentOptVersion(org.onap.policy.models.pap.concepts.PolicyIdentOptVersion source) {
+        IdentUtil.toInternal(source.getName(), source.getVersion(), this);
+    }
+
+    /**
+     * Converts this to the corresponding class in models-pap.
+     *
+     * @return an external object, populated with data from this object
+     */
+    public org.onap.policy.models.pap.concepts.PolicyIdentOptVersion toExternal() {
+
+        return copyTo(new org.onap.policy.models.pap.concepts.PolicyIdentOptVersion());
+    }
+
+    /**
+     * Copies fields from "this" to the target.
+     *
+     * @param target target to which to copy the fields
+     * @return the target
+     */
+    public org.onap.policy.models.pap.concepts.PolicyIdentOptVersion copyTo(
+                    org.onap.policy.models.pap.concepts.PolicyIdentOptVersion target) {
+
+        target.setName(IdentUtil.nameToExternal(this.getName()));
+        target.setVersion(IdentUtil.versionToExternal(this.getVersion()));
+
+        return target;
+    }
+}
