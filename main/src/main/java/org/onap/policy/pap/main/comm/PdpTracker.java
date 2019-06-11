6e1f07e4b6e434229a489584583f696fc1e678c2
jrh3 <jrh3@att.com>
2019-06-11 10:56:25 -0400

    Add PDP heart beat expiration timer
    Added heart beat interval to the PDP-UPDATE message sent in response
    to a heart beat message received from a PDP.
    Added timers to detect missing heart beats and remove the PDP from
    the DB - PdpTracker.
    Modified current heart beat listener to update PdpTracker when a
    heart beat is received.
    
    Change-Id: I81621fefbe494e0c4d6f0b9767b00b2a9dd398d8
    Issue-ID: POLICY-1795
    Signed-off-by: jrh3 <jrh3@att.com>

---
+++ main/src/main/java/org/onap/policy/pap/main/comm/PdpTracker.java
@@ -0,0 +1,152 @@
+/*
+ * ============LICENSE_START=======================================================
+ * ONAP PAP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pap.main.comm;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.Builder;
+import org.onap.policy.models.base.PfModelException;
+import org.onap.policy.models.pdp.concepts.Pdp;
+import org.onap.policy.models.pdp.concepts.PdpGroup;
+import org.onap.policy.models.pdp.concepts.PdpSubGroup;
+import org.onap.policy.models.provider.PolicyModelsProvider;
+import org.onap.policy.pap.main.PolicyModelsProviderFactoryWrapper;
+import org.onap.policy.pap.main.PolicyPapRuntimeException;
+import org.onap.policy.pap.main.comm.TimerManager.Timer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Tracks PDPs. When a PDP is added to the tracker, a timer is started. If the PDP is not
+ * re-added to the tracker before the timer expires, then
+ * {@link PdpModifyRequestMap#removeFromGroups(String)} is called.
+ */
+public class PdpTracker {
+    private static final Logger logger = LoggerFactory.getLogger(PdpTracker.class);
+
+    /**
+     * PDP expiration timers.
+     */
+    private final TimerManager timers;
+
+    /**
+     * Maps a PDP name to its expiration timer.
+     */
+    private final Map<String, TimerManager.Timer> pdp2timer = new HashMap<>();
+
+    /**
+     * PDP modification lock.
+     */
+    private final Object modifyLock;
+
+    /**
+     * Used to remove a PDP from its group/subgroup.
+     */
+    private final PdpModifyRequestMap requestMap;
+
+
+    /**
+     * Constructs the object. Loads the list of PDPs to be tracked, from the DB.
+     *
+     * @param requestMap map used to remove a PDP from its group/subgroup
+     * @param modifyLock object to be locked while data structures are updated
+     * @param timers timers used to detect missed heart beats
+     * @param daoFactory DAO factory
+     */
+    @Builder
+    public PdpTracker(PdpModifyRequestMap requestMap, Object modifyLock, TimerManager timers,
+                    PolicyModelsProviderFactoryWrapper daoFactory) {
+
+        this.requestMap = requestMap;
+        this.modifyLock = modifyLock;
+        this.timers = timers;
+
+        loadPdps(daoFactory);
+    }
+
+    /**
+     * Loads the PDPs from the DB.
+     *
+     * @param daoFactory DAO factory
+     */
+    private void loadPdps(PolicyModelsProviderFactoryWrapper daoFactory) {
+        synchronized (modifyLock) {
+            try (PolicyModelsProvider dao = daoFactory.create()) {
+                for (PdpGroup group : dao.getPdpGroups(null)) {
+                    loadPdpsFromGroup(group);
+                }
+
+            } catch (PfModelException e) {
+                throw new PolicyPapRuntimeException("cannot load PDPs from the DB", e);
+            }
+        }
+    }
+
+    /**
+     * Loads the PDPs appearing within a group.
+     *
+     * @param group group whose PDPs are to be loaded
+     */
+    private void loadPdpsFromGroup(PdpGroup group) {
+        for (PdpSubGroup subgrp : group.getPdpSubgroups()) {
+            for (Pdp pdp : subgrp.getPdpInstances()) {
+                add(pdp.getInstanceId());
+            }
+        }
+    }
+
+    /**
+     * Adds a PDP to the tracker and starts its timer. If a timer is already running, the
+     * old timer is cancelled.
+     *
+     * @param pdpName name of the PDP
+     */
+    public void add(String pdpName) {
+        synchronized (modifyLock) {
+            Timer timer = pdp2timer.remove(pdpName);
+            if (timer != null) {
+                timer.cancel();
+            }
+
+            timer = timers.register(pdpName, this::handleTimeout);
+            pdp2timer.put(pdpName, timer);
+        }
+    }
+
+    /**
+     * Handles a timeout. Removes the PDP from {@link #pdp2timer}.
+     *
+     * @param pdpName name of the PDP whose timer has expired
+     */
+    private void handleTimeout(String pdpName) {
+        synchronized (modifyLock) {
+            // remove timer - no need to cancel it, as TimerManager does that
+            pdp2timer.remove(pdpName);
+
+            try {
+                requestMap.removeFromGroups(pdpName);
+
+            } catch (PfModelException e) {
+                logger.warn("unable to remove PDP {} from its group/subgroup", pdpName, e);
+            }
+        }
+    }
+}
---
+++ main/src/test/java/org/onap/policy/pap/main/comm/PdpTrackerTest.java
@@ -0,0 +1,186 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP PAP
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pap.main.comm;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.onap.policy.common.utils.coder.StandardCoder;
+import org.onap.policy.common.utils.resources.ResourceUtils;
+import org.onap.policy.models.base.PfModelException;
+import org.onap.policy.models.pdp.concepts.PdpGroup;
+import org.onap.policy.models.pdp.concepts.PdpGroups;
+import org.onap.policy.models.provider.PolicyModelsProvider;
+import org.onap.policy.pap.main.PolicyModelsProviderFactoryWrapper;
+import org.onap.policy.pap.main.PolicyPapRuntimeException;
+import org.onap.policy.pap.main.comm.TimerManager.Timer;
+
+public class PdpTrackerTest {
+    private static final String PDP1 = "pdp1";
+    private static final String PDP2 = "pdp2";
+
+    private PdpTracker tracker;
+    private PdpTracker.PdpTrackerBuilder builder;
+
+    private Object modifyLock;
+
+    @Captor
+    private ArgumentCaptor<Consumer<String>> handlerCaptor;
+
+    @Mock
+    private PdpModifyRequestMap requestMap;
+
+    @Mock
+    private TimerManager timers;
+
+    @Mock
+    private PolicyModelsProviderFactoryWrapper daoFactory;
+
+    @Mock
+    private PolicyModelsProvider dao;
+
+    @Mock
+    private Timer timer1;
+
+    @Mock
+    private Timer timer2;
+
+    /**
+     * Sets up.
+     *
+     * @throws Exception if an error occurs
+     */
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        modifyLock = new Object();
+
+        builder = PdpTracker.builder().daoFactory(daoFactory).modifyLock(modifyLock).requestMap(requestMap)
+                        .timers(timers);
+
+        when(daoFactory.create()).thenReturn(dao);
+
+        when(dao.getPdpGroups(null)).thenReturn(Collections.emptyList());
+
+        when(timers.register(eq(PDP1), any())).thenReturn(timer1);
+        when(timers.register(eq(PDP2), any())).thenReturn(timer2);
+
+        tracker = builder.build();
+    }
+
+    @Test
+    public void testPdpTracker() throws Exception {
+        // verify that PDPs were loaded
+        verify(dao).getPdpGroups(null);
+    }
+
+    @Test
+    public void testLoadPdps_testLoadPdpsFromGroup() throws Exception {
+        // arrange for DAO to return a couple of groups
+        String groupsJson = ResourceUtils.getResourceAsString("comm/PdpTracker.json");
+        List<PdpGroup> groups = new StandardCoder().decode(groupsJson, PdpGroups.class).getGroups();
+        when(dao.getPdpGroups(null)).thenReturn(groups);
+
+        tracker = builder.build();
+
+        // verify that all PDPs were registered
+        verify(timers).register(eq("pdp-A"), any());
+        verify(timers).register(eq("pdp-B"), any());
+        verify(timers).register(eq("pdp-C"), any());
+        verify(timers).register(eq("pdp-D"), any());
+    }
+
+    @Test
+    public void testLoadPdps_DaoException() throws Exception {
+        // arrange for DAO to throw an exception
+        PfModelException ex = mock(PfModelException.class);
+        when(daoFactory.create()).thenThrow(ex);
+
+        assertThatThrownBy(() -> builder.build()).isInstanceOf(PolicyPapRuntimeException.class).hasCause(ex);
+    }
+
+    @Test
+    public void testAdd() {
+        tracker.add(PDP1);
+        verify(timers).register(eq(PDP1), any());
+        verify(timer1, never()).cancel();
+
+        tracker.add(PDP2);
+        verify(timers).register(eq(PDP2), any());
+        verify(timer1, never()).cancel();
+        verify(timer2, never()).cancel();
+
+        // re-add PDP1 - old timer should be canceled and a new timer added
+        Timer timer3 = mock(Timer.class);
+        when(timers.register(eq(PDP1), any())).thenReturn(timer3);
+        tracker.add(PDP1);
+        verify(timer1).cancel();
+        verify(timer2, never()).cancel();
+        verify(timer3, never()).cancel();
+    }
+
+    @Test
+    public void testHandleTimeout() throws Exception {
+        tracker.add(PDP1);
+        tracker.add(PDP2);
+
+        verify(timers).register(eq(PDP1), handlerCaptor.capture());
+
+        handlerCaptor.getValue().accept(PDP1);
+
+        verify(requestMap).removeFromGroups(PDP1);
+
+        // now we'll re-add PDP1 - the original timer should not be canceled
+        Timer timer3 = mock(Timer.class);
+        when(timers.register(eq(PDP1), any())).thenReturn(timer3);
+        tracker.add(PDP1);
+        verify(timer1, never()).cancel();
+    }
+
+    @Test
+    public void testHandleTimeout_MapException() throws Exception {
+        tracker.add(PDP1);
+
+        verify(timers).register(eq(PDP1), handlerCaptor.capture());
+
+        // arrange for request map to throw an exception
+        PfModelException ex = mock(PfModelException.class);
+        when(requestMap.removeFromGroups(PDP1)).thenThrow(ex);
+
+        // exception should be caught, but not re-thrown
+        handlerCaptor.getValue().accept(PDP1);
+    }
+}
