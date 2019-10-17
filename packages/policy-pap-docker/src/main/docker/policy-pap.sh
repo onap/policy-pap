2053c23992c40623cf74ad89a5c9875a6d554657
jhh <jorge.hernandez-herrero@att.com>
2019-10-16 19:39:55 -0500

    override cert stores
    Issue-ID: POLICY-2064
    Signed-off-by: jhh <jorge.hernandez-herrero@att.com>
    Change-Id: I9a6077b51c1684b7c34a1cec827288cfb7dc58bc

--- packages/policy-pap-docker/src/main/docker/policy-pap.sh
+++ packages/policy-pap-docker/src/main/docker/policy-pap.sh
@@ -40,4 +40,14 @@ fi
 
 echo "Policy pap config file: $CONFIG_FILE"
 
+if [[ -f "${POLICY_HOME}"/etc/mounted/policy-truststore ]]; then
+    echo "overriding policy-truststore"
+    cp -f "${POLICY_HOME}"/etc/mounted/policy-truststore  "${TRUSTSTORE}"
+fi
+
+if [[ -f "${POLICY_HOME}"/etc/mounted/policy-keystore ]]; then
+    echo "overriding policy-keystore"
+    cp -f "${POLICY_HOME}"/etc/mounted/policy-keystore  "${KEYSTORE}"
+fi
+
 $JAVA_HOME/bin/java -cp "${POLICY_HOME}/etc:${POLICY_HOME}/lib/*" -Djavax.net.ssl.keyStore="$KEYSTORE" -Djavax.net.ssl.keyStorePassword="$KEYSTORE_PASSWD" -Djavax.net.ssl.trustStore="$TRUSTSTORE" -Djavax.net.ssl.trustStorePassword="$TRUSTSTORE_PASSWD" org.onap.policy.pap.main.startstop.Main -c $CONFIG_FILE
