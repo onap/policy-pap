f0d96894bd32a127768ebf4fcc70614341fcac0c
ning.xi <ning.xi@est.tech>
2019-12-26 21:28:20 +0800

    statistics rest api update
    Issue-ID: POLICY-1629
    Signed-off-by: ning.xi <ning.xi@est.tech>
    Change-Id: I104fc448d27a282c01e7f54db300f918374b8d07

---
+++ main/src/main/java/org/onap/policy/pap/main/rest/StatisticsRestProvider.java
@@ -0,0 +1,125 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2020 Nordix Foundation.
+ *  Modifications Copyright (C) 2019 AT&T Intellectual Property.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.policy.pap.main.rest;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.Response;
+import org.onap.policy.common.utils.services.Registry;
+import org.onap.policy.models.base.PfModelException;
+import org.onap.policy.models.base.PfModelRuntimeException;
+import org.onap.policy.models.pdp.concepts.PdpStatistics;
+import org.onap.policy.models.provider.PolicyModelsProvider;
+import org.onap.policy.pap.main.PapConstants;
+import org.onap.policy.pap.main.PolicyModelsProviderFactoryWrapper;
+import org.onap.policy.pap.main.startstop.PapActivator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class to fetch statistics of pap component.
+ *
+ * @author Ram Krishna Verma (ram.krishna.verma@est.tech)
+ */
+public class StatisticsRestProvider {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StatisticsRestProvider.class);
+    private static final String GET_STATISTICS_ERR_MSG = "fetch database failed";
+    private static final String DESC_ORDER = "DESC";
+
+    /**
+     * Returns the current statistics of pap component.
+     *
+     * @return Report containing statistics of pap component
+     */
+    public StatisticsReport fetchCurrentStatistics() {
+        final StatisticsReport report = new StatisticsReport();
+        report.setCode(Registry.get(PapConstants.REG_PAP_ACTIVATOR, PapActivator.class).isAlive() ? 200 : 500);
+
+        PapStatisticsManager mgr = Registry.get(PapConstants.REG_STATISTICS_MANAGER, PapStatisticsManager.class);
+        report.setTotalPdpCount(mgr.getTotalPdpCount());
+        report.setTotalPdpGroupCount(mgr.getTotalPdpGroupCount());
+        report.setTotalPolicyDownloadCount(mgr.getTotalPolicyDownloadCount());
+        report.setPolicyDownloadSuccessCount(mgr.getPolicyDownloadSuccessCount());
+        report.setPolicyDownloadFailureCount(mgr.getPolicyDownloadFailureCount());
+        report.setTotalPolicyDeployCount(mgr.getTotalPolicyDeployCount());
+        report.setPolicyDeploySuccessCount(mgr.getPolicyDeploySuccessCount());
+        report.setPolicyDeployFailureCount(mgr.getPolicyDeployFailureCount());
+
+        return report;
+    }
+
+    /**
+     * Returns statistics of pdp component from database.
+     *
+     * @param groupName name of the PDP group
+     * @param subType type of the sub PDP group
+     * @param pdpName the name of the PDP
+     * @param recordCount the count to query from database
+     * @return Report containing statistics of pdp component
+     * @throws PfModelException when database can not found
+     */
+    public Map<String, Map<String, List<PdpStatistics>>> fetchDatabaseStatistics(String groupName, String subType,
+            String pdpName, int recordCount) throws PfModelException {
+        final PolicyModelsProviderFactoryWrapper modelProviderWrapper =
+                Registry.get(PapConstants.REG_PAP_DAO_FACTORY, PolicyModelsProviderFactoryWrapper.class);
+        Map<String, Map<String, List<PdpStatistics>>> pdpStatisticsMap;
+        try (PolicyModelsProvider databaseProvider = modelProviderWrapper.create()) {
+            Date startTime = null;
+            Date endTime = null;
+
+            if (groupName == null) {
+                pdpStatisticsMap = generatePdpStatistics(databaseProvider.getPdpStatistics(pdpName, startTime));
+            } else {
+                pdpStatisticsMap = generatePdpStatistics(databaseProvider.getFilteredPdpStatistics(pdpName, groupName,
+                        subType, startTime, endTime, DESC_ORDER, recordCount));
+            }
+        } catch (final PfModelException exp) {
+            String errorMessage = GET_STATISTICS_ERR_MSG + "groupName:" + groupName + "subType:" + subType + "pdpName:"
+                    + pdpName + exp.getMessage();
+            LOGGER.debug(errorMessage, exp);
+            throw new PfModelRuntimeException(Response.Status.BAD_REQUEST, errorMessage);
+        }
+        return pdpStatisticsMap;
+    }
+
+    /**
+     * generate the statistics of pap component by group/subgroup.
+     *
+     */
+    private Map<String, Map<String, List<PdpStatistics>>> generatePdpStatistics(List<PdpStatistics> pdpStatisticsList) {
+        Map<String, Map<String, List<PdpStatistics>>> groupMap = new HashMap<>();
+        if (pdpStatisticsList != null) {
+            pdpStatisticsList.stream().forEach(s -> {
+                String curGroup = s.getPdpGroupName();
+                String curSubGroup = s.getPdpSubGroupName();
+                groupMap.computeIfAbsent(curGroup, curGroupMap -> new HashMap<>())
+                        .computeIfAbsent(curSubGroup, curSubGroupList -> new ArrayList<>()).add(s);
+            });
+        }
+        return groupMap;
+    }
+}
+
+
