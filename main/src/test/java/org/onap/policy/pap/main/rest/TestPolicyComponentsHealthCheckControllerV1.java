0a22aed8e0ec01022168457029b68920a5482cb2
huaxing <huaxing.jin@est.tech>
2020-02-25 15:53:55 +0800

    Add test cases for consolidated policy health check
    Issue-ID: POLICY-1689
    Signed-off-by: huaxing <huaxing.jin@est.tech>
    Change-Id: Ifda17a4486e1bce1ce0f523cc1984519b7fbc1aa

--- main/src/test/java/org/onap/policy/pap/main/rest/TestPolicyComponentsHealthCheckControllerV1.java
+++ main/src/test/java/org/onap/policy/pap/main/rest/TestPolicyComponentsHealthCheckControllerV1.java
@@ -1,6 +1,6 @@
 /*-
  * ============LICENSE_START=======================================================
- *  Copyright (C) 2019 Nordix Foundation.
+ *  Copyright (C) 2019-2020 Nordix Foundation.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,25 @@
 
 package org.onap.policy.pap.main.rest;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.onap.policy.common.endpoints.event.comm.bus.internal.BusTopicParams;
+import org.onap.policy.common.parameters.ParameterService;
+import org.onap.policy.pap.main.parameters.PapParameterGroup;
+import org.powermock.reflect.Whitebox;
 
 /**
  * Class to perform unit test of {@link PolicyComponentsHealthCheckControllerV1}.
@@ -30,9 +48,45 @@ import org.junit.Test;
 public class TestPolicyComponentsHealthCheckControllerV1 extends CommonPapRestServer {
 
     private static final String ENDPOINT = "components/healthcheck";
+    private static List<BusTopicParams> savedBusTopicParams;
+
+    /**
+     * Set up for the test class.
+     */
+    @BeforeClass
+    public static void setUpClass() {
+        // To skip calling to the remote components
+        PapParameterGroup papParameterGroup = ParameterService.get("PapGroup");
+        List<BusTopicParams> lo = Whitebox.getInternalState(papParameterGroup,
+            "healthCheckRestClientParameters");
+        savedBusTopicParams = new ArrayList<>(lo);
+        lo.clear();
+    }
+
+    /**
+     * Tear down for the test class.
+     */
+    @AfterClass
+    public static void tearDownClass() {
+        PapParameterGroup papParameterGroup = ParameterService.get("PapGroup");
+        List<BusTopicParams> lo = Whitebox.getInternalState(papParameterGroup,
+            "healthCheckRestClientParameters");
+        lo.addAll(savedBusTopicParams);
+    }
 
     @Test
     public void testSwagger() throws Exception {
         super.testSwagger(ENDPOINT);
+    }
+
+    @Test
+    public void testPolicyComponentsHealthCheck() throws Exception {
+        Invocation.Builder invocationBuilder = sendRequest(ENDPOINT);
+        Response response = invocationBuilder.get();
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        Map<String, Object> result = new HashMap<>();
+        result = (Map<String, Object>) response.readEntity(GenericType.forInstance(result));
+        // No PDP configured, healthy is false
+        assertFalse((Boolean) result.get("healthy"));
     }
 }
