ed600462152536a1ff31ae96732b61e947cd5ba4
Jim Hahn <jrh3@att.com>
2020-04-07 13:34:34 -0400

    Address sonar issues in PAP
    Addressed the following sonar issues in PAP:
    - do something with value returned by a method
    - use computeIfAbsent; chose to disable the sonar for this,
      as it would obfuscate the code
    - use RE2 instead of java.util Pattern
    
    Issue-ID: POLICY-2305
    Change-Id: If5495d04248211179cd37419cb20fe7d227ab59c
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- main/src/main/java/org/onap/policy/pap/main/comm/Publisher.java
+++ main/src/main/java/org/onap/policy/pap/main/comm/Publisher.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP PAP
  * ================================================================================
- * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2019-2020 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,7 +103,7 @@ public class Publisher<T> implements Runnable {
 
             if (stopNow) {
                 // unblock any other publisher threads
-                queue.offer(new QueueToken<>(null));
+                queue.add(new QueueToken<>(null));
                 break;
             }
 
