018eabfdf2b2ebc6f3444a671e14dcf7d8902c76
Jim Hahn <jrh3@att.com>
2020-06-30 07:31:20 -0400

    Fix more sonars in pap
    Somehow missed this sonar on one line of code:
    - remove useless assignments
    
    Issue-ID: POLICY-2679
    Change-Id: I270341195634416db1f8152a95a5822c8d505803
    Signed-off-by: Jim Hahn <jrh3@att.com>

--- main/src/main/java/org/onap/policy/pap/main/rest/PolicyComponentsHealthCheckProvider.java
+++ main/src/main/java/org/onap/policy/pap/main/rest/PolicyComponentsHealthCheckProvider.java
@@ -1,6 +1,7 @@
 /*-
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2019-2020 Nordix Foundation.
+ *  Modifications Copyright (C) 2020 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -99,7 +100,7 @@ public class PolicyComponentsHealthCheckProvider {
      */
     public Pair<Status, Map<String, Object>> fetchPolicyComponentsHealthStatus() {
         boolean isHealthy = true;
-        Map<String, Object> result = new HashMap<>();
+        Map<String, Object> result;
 
         // Check remote components
         List<Callable<Entry<String, Object>>> tasks = new ArrayList<>();
